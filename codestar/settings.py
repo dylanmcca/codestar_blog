4d2bb754d3e6a869d703d9fea267f39f9c38836b
dylanmcca <dylanmcca@gmail.com>
2025-11-08 13:34:44 +0000

    adding urls path & other code

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!ux+-kehnr8(dst&2#@ibu1$)l18x-f0n)sdldpdjo=3h19(_+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','http://127.0.0.1:8000/']
 
 
 # Application definition
