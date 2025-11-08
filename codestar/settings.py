c1edd0892ff5c1b956832dad5c3848e258ac7e17
dylanmcca <dylanmcca@gmail.com>
2025-11-08 15:40:25 +0000

    trying to link to db

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!ux+-kehnr8(dst&2#@ibu1$)l18x-f0n)sdldpdjo=3h19(_+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com','http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -74,11 +78,15 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("postgresql://neondb_owner:npg_O54cmZRpqvCo@ep-polished-pond-ag9l10ay.c-2.eu-central-1.aws.neon.tech/claim_sweat_blast_946183"))
 }
 
 
--- codestar/urls.py
+++ codestar/urls.py
@@ -20,5 +20,5 @@ from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog_views.my_blog, name="blog"), # the app urls are loaded as the main urls
+    path('blog/', blog_views.my_blog, name="blog"),
 ]
